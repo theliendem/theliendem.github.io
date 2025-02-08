e5b183f09ea45303a62974140c871c88dfe813e8
iNeil Mac <neil@neildembla.com>
2025-02-07 19:12:10 -0500

    added logo, added basic framework for the rest of the pages (about, contact, hobbies & projects), and the basic_page.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This is the general script used in every page. Individual pages can have their own scripts as well.
+
 // Bootstrap
 bs_link = document.createElement('link')
 	.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"

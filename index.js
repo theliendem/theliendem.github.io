6a7b9614d23eb1295322a87990ac11bf4e125ce4
iNeil Mac <neil@neildembla.com>
2025-03-30 22:52:10 -0400

    major visual overhaul, step 1: index page

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const content = document.getElementById("hidden-content").innerHTML;
+document.getElementById("hidden-content").style.display = "none";
 
-const phrases = ["</Hello, world!>", "I'm Neil!"];
-const el = document.getElementById("typewriter");
+// async function navbarStuff() {
+// 	const nav = document.querySelector("nav");
+// 	nav.style.display = "none";
+// 	nav.style.opacity = 0;
+// 	nav.style.display = "block";
+// 	nav.style.transition = "opacity 0.5s ease-in-out";
+// }
 
-let sleepTime = 100;
-let curPhraseIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+	// navbarStuff();
 
-const writeLoop = async () => {
-	while (true) {
-		let curWord = phrases[curPhraseIndex]
+	const div = document.getElementById("content");
 
-		for (let i = 0; i < curWord.length; i++) {
-			el.innerText = curWord.substring(0, i + 1);
-			await sleep(sleepTime);
+	let i = 0;
+	let inTag = false;
+
+	function typeNextCharacter() {
+		if (i < content.length) {
+			const char = content[i];
+
+			// Update inTag status
+			if (char === '<') {
+				inTag = true;
+			} else if (char === '>') {
+				inTag = false;
+			}
+
+			// Set typing speed based on whether we're in a tag
+			const speed = inTag ? 0 : 35; // Fast for tags, slow for visible text
+
+			// Update the content
+			div.innerHTML = content.substring(0, i + 1) + `<span id="cursor">_</span>`;
+
+			// Schedule the next character
+			i++;
+			setTimeout(typeNextCharacter, speed);
 		}
 
-		await sleep(sleepTime * 26);
-
-		for (let i = curWord.length; i > 0; i--) {
-			el.innerText = curWord.substring(0, i - 1);
-			await sleep(sleepTime);
-		}
-
-		await sleep(sleepTime * 10)
-
-		if (curPhraseIndex === phrases.length - 1) curPhraseIndex = 0;
-		else curPhraseIndex++;
 	}
-};
+	
+	// Start the typing animation
+	typeNextCharacter();
 
-writeLoop();
+	document.getElementById("cursor").innerHTML = "";
+});
+
+// document.addEventListener("DOMContentLoaded", function() {
+// 	const length = content.length;
+// 	const div = document.getElementById("content");
+
+// 	for (let i = 0; i < length; i++) {
+// 		let newContent = content.substring(0, i);
+// 		let speed = 50;
+
+// 		setTimeout(() => {
+// 			div.innerHTML = newContent;
+
+// 			if (content.substring(i, i + 1) === "<") speed = 50;
+// 			else if (content.substring(i, i + 1) === ">") speed = 10;
+
+// 			console.log(speed);
+// 		}, speed * i);
+// 	}
+// });
